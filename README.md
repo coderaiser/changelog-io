95ba1b662391fa929e6590aa58f2253d694084ba
coderaiser <mnemonic.enemy@gmail.com>
2014-10-29 12:23:02 -0400

    feature(changelog) add

---
+++ README.md
@@ -0,0 +1,32 @@
+# ChangeLog
+
+Generate changelog with one command.
+
+## Install
+
+```
+npm i changelog-io -g
+```
+
+## How it works?
+
+If you already have tags and use conventions like this:
+
+- feature(<scrope>) <command message>
+- fix(<scope>) <commit message>
+- docs(<scope>) <commit message>
+- chore(<scope>) <commit message>
+- refactor(<scope>) <commit message>
+
+`feature` and `fix` commit messages from previous
+tag would be got and puted to `ChangeLog` file.
+
+## How to use?
+
+```
+changelog -v <version>
+```
+
+## License
+
+MIT
---
+++ bin/changelog.js
@@ -0,0 +1,92 @@
+#!/usr/bin/env node
+
+(function() {
+    'use strict';
+    
+    var DIR         = process.cwd(),
+        fs          = require('fs'),
+        exec        = require('child_process').exec,
+        
+        Util        = require('util-io'),
+        
+        argv        = require('minimist')(process.argv.slice(2)),
+        
+        tryRequire  = require('../lib/tryRequire'),
+        
+        Info        = tryRequire(DIR + '/package', {log: true});
+        
+    if (Info)
+        start(argv.v || argv.version);
+            
+    function start(versionNew) {
+        var isV         = /^v/.test(versionNew),
+            version     = 'v' + Info.version,
+            name        = 'ChangeLog',
+            
+            gitTempl    = 'git log {{ version }}..HEAD --pretty=format:"- %s" --grep {{ category }} | sed  \'s/{{ category }}//g\'',
+            
+            gitFix      = Util.render(gitTempl, {
+               category : 'fix' ,
+               version  : version
+            }),
+            
+            gitFeature  = Util.render(gitTempl, {
+               category: 'feature',
+               version  : version
+            });
+        
+        if (!isV && versionNew)
+            versionNew  = 'v' + versionNew;
+        
+        fs.readFile(name, 'utf8', function(error, fileData) {
+            Util.exec.parallel([
+                Util.exec.with(exec, gitFix),
+                Util.exec.with(exec, gitFeature),
+                ], function(error, fixData, featureData) {
+                    var fix, feature,
+                        DATA        = 0,
+                        STD_ERR     = 1,
+                        date        = Util.getShortDate(),
+                        head        = date + ', ' + versionNew + '\n\n',
+                        data        = '';
+                    
+                    if (!error) {
+                        fix         = fixData[DATA];
+                        feature     = featureData[DATA];
+                    }
+                    
+                    if (fix || feature) {
+                        data        = head;
+                        
+                        if (fix) {
+                            data    += 'fix:'       + '\n';
+                            data    += fix          + '\n';
+                        }
+                        
+                        if (feature) {
+                            data    += 'feature:'   + '\n';
+                            data    += feature      + '\n';
+                        }
+                        
+                        if (fileData)
+                            data        += '\n\n' + fileData;
+                    }
+                    
+                    error   = error || fixData[STD_ERR] || featureData[STD_ERR];
+                    
+                    if (error)
+                        console.error(error);
+                    else if (!data)
+                        console.log('No new feature and fix commits from v', version);
+                    else if (!versionNew)
+                        console.log(data);
+                    else
+                        fs.writeFile(name, data, function(error) {
+                                var msg = 'changelog: done';
+                                
+                                console.log(error || msg);
+                            });
+                });
+        });
+    }
+})();
