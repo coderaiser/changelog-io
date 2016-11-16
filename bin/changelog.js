02685cfc1a3b9a5a03582675636a1503c6aaf2f0
coderaiser <coderaiser@cloudcmd.io>
2016-11-16 12:48:38 +0200

    chore(changelog) rm iife

--- bin/changelog.js
+++ bin/changelog.js
@@ -1,15 +1,13 @@
 #!/usr/bin/env node
 
-(function() {
-    'use strict';
-    
-    var changelog   = require('../lib/changelog'),
-        argv        = require('minimist')(process.argv.slice(2));
-        
-        changelog(argv._[0], function(error, msg) {
-            if (error)
-                console.error(error.message);
-            else
-                console.log(msg);
-        });
-})();
+'use strict';
+
+var changelog   = require('../lib/changelog'),
+    argv        = require('minimist')(process.argv.slice(2));
+
+changelog(argv._[0], function(error, msg) {
+    if (error)
+        console.error(error.message);
+    else
+        console.log(msg);
+});
