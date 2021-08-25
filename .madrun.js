e88a2be6b1a7ea7e4549fa9252a89b3d810b3541
coderaiser <coderaiser@cloudcmd.io>
2021-08-25 11:57:20 +0300

    feature(changelog) convert ot ESM

--- .madrun.js
+++ .madrun.js
@@ -3,5 +3,7 @@ import {run} from 'madrun';
 export default {
     'lint': () => 'putout .',
     'fix:lint': () => run('lint', '--fix'),
+    'test': () => `tape 'test/**/*.js' 'lib/**/*.spec.js'`,
+    'coverage': async () => `c8 ${await run('test')}`,
 };
 
