ec873b9de0069e1e03308e78092b871817185b56
coderaiser <coderaiser@cloudcmd.io>
2024-04-28 23:31:07 +0300

    feature: changelog-io: escover -> c8

--- .madrun.js
+++ .madrun.js
@@ -2,16 +2,10 @@ import {
     run,
     cutEnv,
 } from 'madrun';
-import {SKIPED} from 'supertape/exit-codes';
-
-const env = {
-    ESCOVER_SUCCESS_EXIT_CODE: SKIPED,
-    SUPERTAPE_CHECK_SKIPED: 1,
-};
 
 export default {
-    'test': () => [env, `escover tape 'test/**/*.js' 'lib/**/*.spec.js'`],
-    'coverage': async () => [env, await cutEnv('test')],
+    'test': () => `tape 'test/**/*.js' 'lib/**/*.spec.js'`,
+    'coverage': async () => `c8 ${await cutEnv('test')}`,
     'inspect': async () => [env, `node-inspect lib/changelog.spec.js`],
     'lint': () => 'putout .',
     'fresh:lint': () => run('lint', '--fresh'),
